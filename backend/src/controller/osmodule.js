706b0f44661dba6af631755bb0707c347341f4f3
LucasGuerraCavalcante <lucasguerratee@gmail.com>
2020-04-27 03:56:34 -0300

    api returns day and time

--- backend/src/controller/osmodule.js
+++ backend/src/controller/osmodule.js
@@ -14,6 +14,13 @@ module.exports = {
         const release = os.release()
         const type = os.type()
 
+        // https://www.w3resource.com/javascript/object-property-method/date-getDay.php
+        const weekDay = new Date().getDay()
+        const day = new Date().getDate()
+        const month = new Date().getMonth()
+        const year = new Date().getFullYear()
+        const timezone = new Date().getTimezoneOffset()
+
         // const networkInterfaces = os.networkInterfaces()
 
         // const cpus = os.cpus()
@@ -39,7 +46,13 @@ module.exports = {
             platform: platform,
             hostname: hostname,
             release: release,
-            type: type
+            type: type,
+            weekDay: weekDay,
+            day: day,
+            month: month,
+            year: year,
+            timezone: timezone
+
         
         }
 
