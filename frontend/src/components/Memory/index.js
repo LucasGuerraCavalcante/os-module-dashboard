51ad21540836022a68b6f80841187f6308c68795
LucasGuerraCavalcante <lucasguerratee@gmail.com>
2020-04-30 21:18:15 -0300

    adding the bar chart

--- frontend/src/components/Memory/index.js
+++ frontend/src/components/Memory/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import './style.css'
 
 import axios from 'axios';
+import Paper from '@material-ui/core/Paper';
+import { Chart, BarSeries, Title, ArgumentAxis, ValueAxis,} from '@devexpress/dx-react-chart-material-ui';
+import { Animation } from '@devexpress/dx-react-chart';
 
 export default class Memory extends React.Component {
 
-  state = { free: 0, usage: 0, total: 0 }
+  state = { free: 0, usage: 0, total: 0, data: [{lable: 'usage', memory: 0},{lable: 'free', memory: 0}]}
 
   componentDidMount() {
     try {
@@ -15,7 +18,9 @@ export default class Memory extends React.Component {
             const free = res.data.memory.free;
             const usage = res.data.memory.usage;
             const total = res.data.memory.total;
-            this.setState({ free, usage, total });
+            const data = [{lable: 'Usage', memory: usage},{lable: 'Free', memory: free}]
+             
+            this.setState({ free, usage, total, data });
         })
       }, 1000)
     } catch(err) {
@@ -30,6 +35,23 @@ export default class Memory extends React.Component {
         <p>{this.state.free}</p>
         <p>{this.state.usage}</p>
         <p>{this.state.total}</p>
+        <div>
+          <Paper>
+            <Chart
+              data={this.state.data}
+              >
+                <ArgumentAxis />
+                  <ValueAxis max={2} />
+
+                  <BarSeries
+                    valueField="memory"
+                    argumentField="lable"
+                  />
+                <Title text="Memory Usage" />
+              <Animation />
+            </Chart>
+          </Paper>
+        </div>
       </div>
     )
   }
